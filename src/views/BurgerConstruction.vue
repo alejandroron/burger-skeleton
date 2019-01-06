0617a7e5a42195a7a471e9df879820cdcb0b5cfd
asdf <yeramc0227@naver.com>
2019-01-06 16:26:05 +0100

    added customized menu and sides sample

---
+++ src/views/BurgerConstruction.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+<Bars :treedata="tree"
+:meatdata="meat"
+:ingredientsdata="ingredients"
+:saucesdata="sauces"
+></Bars>
+</div>
+</template>
+
+<script>
+
+import Bars from './Test1.vue'
+
+export default {
+  name: 'Test',
+  data: ()=> ({
+  
+tree: {
+  name: "BREAD",
+  children: [
+    {
+      name: "Flat Bread"},{
+      name: "Fat Bread"},{
+      name: "Light Bread"},{
+      name: "Ugly Bread"},{
+      name: "Cute Bread"}
+     
+    
+  ]
+},
+meat:{
+ name: "MEAT",
+  children: [
+    {
+      name: "Flat Bread"},{
+      name: "Fat Bread"},{
+      name: "Light Bread"},{
+      name: "Ugly Bread"},{
+      name: "Cute Bread"}
+     
+    
+  ]
+},
+ingredients:{
+ name: "INGREDIENTS",
+  children: [
+    {
+      name: "Flat Bread"},{
+      name: "Fat Bread"},{
+      name: "Light Bread"},{
+      name: "Ugly Bread"},{
+      name: "Cute Bread"}
+     
+    
+  ]
+},
+sauces:{
+ name: "SAUCES",
+  children: [
+    {
+      name: "Flat Bread"},{
+      name: "Fat Bread"},{
+      name: "Light Bread"},{
+      name: "Ugly Bread"},{
+      name: "Cute Bread"}
+     
+    
+  ]
+}
+  }),
+    components:{
+    Bars,
+  },
+}
+</script>
